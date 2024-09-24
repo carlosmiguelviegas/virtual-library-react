0038e0917b4d0f22c2f2ea4cc98987c71e048ed4
unknown <carlosmviegas86@gmail.com>
2024-09-24 16:00:24 +0100

    implemented login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,6 +1,34 @@
+import { useState } from 'react';
+
+import './Login.css';
+
 const Login = () => {
+
+  const [ loginForm, setLoginForm ] = useState({ email: '', password: '' });
+
+  const handlerOnChange = (input, value) => {
+    setLoginForm(
+      {
+        ...loginForm,
+        [input]: value
+      }
+    );
+  };
+
+  const handlerOnSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
-    <h2>Login</h2>
+    <form className="form" onSubmit={handlerOnSubmit}>
+      <section className='form-container'>
+        <label htmlFor='email'>Email</label>
+        <input type='email' id='email' value={loginForm['email']} onChange={(event) => handlerOnChange('email', event['target']['value'])} />
+        <label htmlFor='password'>Password</label>
+        <input type='password' id='password' value={loginForm['password']} onChange={(event) => handlerOnChange('password', event['target']['value'])} />
+        <button type='submit'>Sign in</button>
+      </section>
+    </form>
   );
 };
 
