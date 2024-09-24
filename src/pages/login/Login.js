924ce3a828082245dbccfa7303a38d0c6ceac046
unknown <carlosmviegas86@gmail.com>
2024-09-24 21:14:34 +0100

    perform http call to login and redirect to home after successfully log the user in

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,31 +1,44 @@
 import { useState } from 'react';
 
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
 import './Login.css';
+import api from './../../utils/api';
+
+const LOGIN_URL = 'http://localhost:8000/api/v1/users/login';
 
 const Login = () => {
 
   const [ loginForm, setLoginForm ] = useState({ email: '', password: '' });
+  const navigate = useNavigate();
 
-  const handlerOnChange = (input, value) => {
+  const handlerOnChange = (event) => {
     setLoginForm(
       {
         ...loginForm,
-        [input]: value
+        [event['target']['name']]: event['target']['value']
       }
     );
   };
 
   const handlerOnSubmit = (event) => {
     event.preventDefault();
+    axios.post(LOGIN_URL, loginForm)
+    .then((res) => {
+      api['token'] = res['headers'].get('token');
+      navigate('/home');
+    })
+    .catch(err => console.log(err));
   };
 
   return (
     <form className="form" onSubmit={handlerOnSubmit}>
       <section className='form-container'>
         <label htmlFor='email'>Email</label>
-        <input type='email' id='email' value={loginForm['email']} onChange={(event) => handlerOnChange('email', event['target']['value'])} />
+        <input type='email' name='email' id='email' value={loginForm['email']} onChange={handlerOnChange} />
         <label htmlFor='password'>Password</label>
-        <input type='password' id='password' value={loginForm['password']} onChange={(event) => handlerOnChange('password', event['target']['value'])} />
+        <input type='password' name='password' id='password' value={loginForm['password']} onChange={handlerOnChange} />
         <button type='submit'>Sign in</button>
       </section>
     </form>
