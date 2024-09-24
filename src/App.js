38a9c2d90f0061fb70ddf898a693281c7d228464
unknown <carlosmviegas86@gmail.com>
2024-09-24 14:03:18 +0100

    configured routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import { Fragment } from 'react';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Users from './pages/users/Users';
+import Books from './pages/books/Books';
+import Login from './pages/login/Login';
+import Home from './pages/home/Home';
 
 const App = () => {
   
   return (
-    <h2>Library</h2>
+    <Fragment>
+      <h2>Library</h2>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='users' element={<Users />}></Route>
+          <Route path='books' element={<Books />}></Route>
+          <Route path='login' element={<Login />}></Route>
+          <Route path='*' element={<Home />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </Fragment>
   );
 
 };
