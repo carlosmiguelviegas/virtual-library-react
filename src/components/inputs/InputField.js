2987e4eb98dc987da869d4c87e2668599fccee77
unknown <carlosmviegas86@gmail.com>
2024-12-23 21:47:42 +0000

    changed input field component css style

--- src/components/inputs/InputField.js
+++ src/components/inputs/InputField.js
@@ -1,11 +1,11 @@
 import styles from './InputField.module.css';
 
-const InputField = ({ type = 'text', label, name, value, handlerOnChange }) => {
+const InputField = ({ type = 'text', label, name, value, width = 189, handlerOnChange }) => {
 
   return (
-    <section className={styles.formGroup}>
-      <label>{ label }</label>
-      <input className={styles.input} type={type} name={name} value={value} onChange={handlerOnChange} />
+    <section className={styles.formControl}>
+      <label className={styles.label}>{ label }</label>
+      <input className={styles.input} type={type} name={name} value={value} style={{width:` ${width}px`}} onChange={handlerOnChange} />
     </section>
   );
 
