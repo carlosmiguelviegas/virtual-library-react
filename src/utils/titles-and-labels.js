2efc33c09fc0bca24856c23c8d566b1af1f28db5
unknown <carlosmviegas86@gmail.com>
2024-12-23 22:10:29 +0000

    changed home css style

--- src/utils/titles-and-labels.js
+++ src/utils/titles-and-labels.js
@@ -3,11 +3,13 @@ const SIGN_IN_LABEL = 'Sign in';
 const SIGN_IN_EMAIL_LABEL = 'Email';
 const SIGN_IN_PASSWORD_LABEL = 'Password';
 const SIGN_IN_SHOW_PASSWORD_LABEL = 'Show Password';
+const HOME_PAGE_WELCOME = 'Welcome';
 
 export {  
           SIGN_IN_TITLE,
           SIGN_IN_LABEL,
           SIGN_IN_EMAIL_LABEL,
           SIGN_IN_PASSWORD_LABEL,
-          SIGN_IN_SHOW_PASSWORD_LABEL
+          SIGN_IN_SHOW_PASSWORD_LABEL,
+          HOME_PAGE_WELCOME
        };
