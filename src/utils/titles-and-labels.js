3eff40228631edf9bbbf7fa97f66c48b55c7627a
unknown <carlosmviegas86@gmail.com>
2024-12-23 17:37:12 +0000

    created titles and labels file

---
+++ src/utils/titles-and-labels.js
@@ -0,0 +1,13 @@
+const SIGN_IN_TITLE = 'Sign in';
+const SIGN_IN_LABEL = 'Sign in';
+const SIGN_IN_EMAIL_LABEL = 'Email';
+const SIGN_IN_PASSWORD_LABEL = 'Password';
+const SIGN_IN_SHOW_PASSWORD_LABEL = 'Show Password';
+
+export {  
+          SIGN_IN_TITLE,
+          SIGN_IN_LABEL,
+          SIGN_IN_EMAIL_LABEL,
+          SIGN_IN_PASSWORD_LABEL,
+          SIGN_IN_SHOW_PASSWORD_LABEL
+       };
