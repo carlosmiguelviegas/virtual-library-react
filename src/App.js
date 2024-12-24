c1dfb59c63c13ad67ce38cc86acbb0f77f76919e
unknown <carlosmviegas86@gmail.com>
2024-12-24 15:47:24 +0000

    added the navigation bar component to app component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Users from './pages/users/Users';
 import Books from './pages/books/Books';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
+import NavigationBar from './components/layout/navigation-bar/NavigationBar';
 
 const App = () => {
 
   const [ currentUser, setCurrentUser ] = useState({});
 
   return (
+    <Fragment>
+      <NavigationBar currentUser={currentUser} />
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home />}></Route>
@@ -20,6 +23,7 @@ const App = () => {
           <Route path='*' element={<Home />}></Route>
         </Routes>
       </BrowserRouter>
+    </Fragment>
   );
 
 };
