9ae6ec8f236406befca29ae6d73690bb3862fd78
unknown <carlosmviegas86@gmail.com>
2024-12-24 16:07:40 +0000

    added the labels for the navigation bar component links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Books from './pages/books/Books';
 import Login from './pages/login/Login';
 import Home from './pages/home/Home';
 import NavigationBar from './components/layout/navigation-bar/NavigationBar';
+import { BOOKS_LINK, HOME_LINK, USERS_LINK } from './utils/titles-and-labels';
 
 const App = () => {
 
