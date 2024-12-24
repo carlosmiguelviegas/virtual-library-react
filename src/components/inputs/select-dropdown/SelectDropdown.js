35fd3160373d2052b62e178217a583b37eb70665
unknown <carlosmviegas86@gmail.com>
2024-12-24 15:13:28 +0000

    implemented reusable select dropdown component

--- src/components/inputs/select-dropdown/SelectDropdown.js
+++ src/components/inputs/select-dropdown/SelectDropdown.js
@@ -1,5 +1,24 @@
-const SelectDropdown = () => {
-  return ;
+import styles from './SelectDropdown.module.css';
+
+const SelectDropdown = ({ label, list, handlerOnChange }) => {
+
+  const optionsToDisplay = list
+    .map(option => 
+      <option style={{backgroundColor: blanchedalmond}} value={option['code']} onChange={handlerOnChange}>
+        {option['label']}
+      </option>
+    );
+
+  return (
+    <div className={styles.formControl}>
+      <label className={styles.label}>{label}</label>
+      <select className={styles.select}>
+      <section>
+        {optionsToDisplay}
+      </section>
+    </select>
+  </div>
+  );
 };
 
 export default SelectDropdown;
