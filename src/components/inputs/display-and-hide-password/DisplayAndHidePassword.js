5f6b82d5eec1a0c6b3355cac715a96ec5bdcdfef
unknown <carlosmviegas86@gmail.com>
2024-12-26 23:04:14 +0000

    implemented the reusable component to display and hide the password

--- src/components/inputs/display-and-hide-password/DisplayAndHidePassword.js
+++ src/components/inputs/display-and-hide-password/DisplayAndHidePassword.js
@@ -1,8 +1,14 @@
+import { SIGN_IN_SHOW_PASSWORD_LABEL } from '../../../utils/titles-and-labels';
 import styles from './DisplayAndHidePassword.module.css';
 
-const DisplayAndHidePassword = () => {
+const DisplayAndHidePassword = ({ value, handlerOnChange }) => {
 
-  return ;
+  return (
+    <section className={styles.showPasswordInput}>
+      <input type={checkbox} value={value} onChange={handlerOnChange} />
+      <p className={styles.showPasswordLabel}>{SIGN_IN_SHOW_PASSWORD_LABEL}</p>
+    </section>
+  );
 
 };
 
