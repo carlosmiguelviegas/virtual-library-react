a58ed73073cf7e8a3c8ecaf5761322430f11ff8e
unknown <carlosmviegas86@gmail.com>
2024-12-27 15:41:07 +0000

    set initial state for the register form

--- src/pages/register/Register.js
+++ src/pages/register/Register.js
@@ -1,5 +1,29 @@
+import { useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+import InputField from '../../components/inputs/input-field/InputField';
+import styles from './Register.module.css';
+
+const SIGN_UP_URL = 'http://localhost:8000/api/v1/users/signup';
+
 const Register = () => {
+
+  const [ registerForm, setRegisterForm ] = useState({ name: '', email: '', password: '', passwordConfirm: '' });
+  const [ showPassword, setShowPassword ] = useState(false);
+  const navigate = useNavigate();
+
+  const handlerOnChange = event => {
+    // it was intentional
+  };
+
+  const handlerOnChangePassword = () => setShowPassword(showPassword => !showPassword);
+
+  const onRegisterNewUser = () => {
+    // it was intentional
+  };
+
   return ;
+
 };
 
 export default Register;
