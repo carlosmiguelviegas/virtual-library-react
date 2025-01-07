ea50c5064ee52a5ccf2cc65520802da81feaa1f5
unknown <carlosmviegas86@gmail.com>
2025-01-07 16:02:48 +0000

    refactored the next page and previous page methods in paginator component to just 1 method, chenge page number, that receives the value to increment or decrement

--- src/components/layout/paginator/Paginator.js
+++ src/components/layout/paginator/Paginator.js
@@ -1,17 +1,18 @@
-import { PAGINATOR_ITEMS_PAGE, PAGINATOR_PAGE_NUMBER } from '../../../utils/titles-and-labels';
+import { PAGINATOR_ITEMS_PAGE, PAGINATOR_PAGE_NUMBER as paginatorPageNUmber } from '../../../utils/titles-and-labels';
 import styles from './Paginator.module.css';
 import { MdOutlineChevronLeft, MdOutlineChevronRight } from "react-icons/md";
 
 const Paginator = ({ pageEvent, totalElements, pageEventChangeHandler }) => {
 
   const { pageIndex, pageSize } = pageEvent;
+
   let totalNumberPages = Math.ceil(totalElements / pageSize);
-  let PAGE_NUMBER_LABEL = PAGINATOR_PAGE_NUMBER(pageIndex, totalNumberPages);
+  let pageNumberLabel = paginatorPageNUmber(pageIndex, totalNumberPages);
 
   const changePageNumber = index => {
     pageEvent = { ...pageEvent,
                   pageIndex: pageIndex + index};
-    PAGE_NUMBER_LABEL = PAGINATOR_PAGE_NUMBER(pageIndex, totalNumberPages);
+    pageNumberLabel = paginatorPageNUmber(pageIndex, totalNumberPages);
     pageEventChangeHandler(pageEvent);
   };
 
@@ -29,7 +30,7 @@ const Paginator = ({ pageEvent, totalElements, pageEventChangeHandler }) => {
         <option className={styles.option} value={8}>8</option>
         <option className={styles.option} value={12}>12</option>
       </select>
-      <label className={styles.pageNumber}>{PAGE_NUMBER_LABEL}</label>
+      <label className={styles.pageNumber}>{pageNumberLabel}</label>
       <section className={styles.buttonsSection}>
         <MdOutlineChevronLeft className={`${styles.icon} ${pageEvent['pageIndex'] === 1 ? styles.disabled : ''}`} onClick={() => changePageNumber(-1)} />
         <MdOutlineChevronRight className={`${styles.icon} ${pageEvent['pageIndex'] === totalNumberPages ? styles.disabled : ''}`} onClick={() => changePageNumber(1)} />
