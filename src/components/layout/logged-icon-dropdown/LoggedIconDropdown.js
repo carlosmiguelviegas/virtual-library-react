b25fdebadfaea780824c3dbd104e0d1c74f77360
unknown <carlosmviegas86@gmail.com>
2025-01-08 18:11:55 +0000

    added JSX template to logged icon dropdown

--- src/components/layout/logged-icon-dropdown/LoggedIconDropdown.js
+++ src/components/layout/logged-icon-dropdown/LoggedIconDropdown.js
@@ -1,9 +1,11 @@
+import { LOGOUT_LINK } from '../../../utils/titles-and-labels';
 import styles from './LoggedIconDropdown.module.css';
 
 const LoggedIconDropdown = ({ onLogout }) => {
   return (
     <section className={styles.dropdownContainer}>
-      
+      <span className={styles.option} onClick={}><p>{}</p></span>
+      <span className={styles.option} onClick={onLogout}><p>{LOGOUT_LINK}</p></span>
     </section>
   );
 };
