2bc0e9d05aab0ce838ce08f512150d9f930d0cae
unknown <carlosmviegas86@gmail.com>
2025-01-09 17:57:24 +0000

    added a logout icon

--- src/components/layout/logged-icon-dropdown/LoggedIconDropdown.js
+++ src/components/layout/logged-icon-dropdown/LoggedIconDropdown.js
@@ -1,5 +1,6 @@
 import { ICON_DROPDOWN_ADMIN_ROLE, ICON_DROPDOWN_LOGOUT_LINK, ICON_DROPDOWN_PROFILE_LINK, ICON_DROPDOWN_USER_ROLE } from '../../../utils/titles-and-labels';
 import styles from './LoggedIconDropdown.module.css';
+import { IoExitOutline } from "react-icons/io5";
 
 const LoggedIconDropdown = ({ currentUser, onLinkClick, onLogout }) => {
   return (
@@ -7,7 +8,10 @@ const LoggedIconDropdown = ({ currentUser, onLinkClick, onLogout }) => {
       <p className={`${styles.userInfo} ${styles.name}`}>{currentUser['name']}</p>
       <p className={`${styles.userInfo} ${styles.property}`}>{'admin' === currentUser['role'] ? ICON_DROPDOWN_ADMIN_ROLE : ICON_DROPDOWN_USER_ROLE}</p>
       <span className={styles.option} onClick={onLinkClick}><p className={styles.optionButton}>{ICON_DROPDOWN_PROFILE_LINK}</p></span>
-      <span className={styles.option} onClick={onLogout}><p className={styles.optionButton}>{ICON_DROPDOWN_LOGOUT_LINK}</p></span>
+      <span className={styles.option} onClick={onLogout}>
+        <IoExitOutline />
+        <p className={styles.optionButton}>{ICON_DROPDOWN_LOGOUT_LINK}</p>
+      </span>
     </section>
   );
 };
