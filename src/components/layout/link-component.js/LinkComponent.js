ec7e7084caca68b470780053e12a705cf6ab5c55
unknown <carlosmviegas86@gmail.com>
2025-01-10 10:30:44 +0000

    implemented the link component JSX and pros to receive

--- src/components/layout/link-component.js/LinkComponent.js
+++ src/components/layout/link-component.js/LinkComponent.js
@@ -1,5 +1,11 @@
-const LinkComponent = () => {
-  return ;
+const LinkComponent = ({ link, onLinkClick }) => {
+
+  return (
+    <span className={styles.linkContainer} key={link['path']} onClick={() => onLinkClick(link['path'])}>
+      {link['icon']}<button className={`${styles.ankor} ${styles.link}`}>{link['path']}</button>
+    </span>
+  );
+
 };
 
 export default LinkComponent;
