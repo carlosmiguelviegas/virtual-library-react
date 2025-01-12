4f7db70897bd7223f74f96da5cef1841f18d4ff3
unknown <carlosmviegas86@gmail.com>
2025-01-12 17:42:47 +0000

    added the store provider to the index.js file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { Provider } from 'react-redux';
 import App from './App';
+import Store from './store/store';
 import './index.css';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 root.render(
-  <App />
+  <Provider store={Store} >
+    <App />
+  </Provider>
 );
