bb5da6ea2495d6bfe5ca337d5d5a700714cadaaf
unknown <carlosmviegas86@gmail.com>
2025-01-14 15:46:05 +0000

    updated the interceptor to retrieve the token from the state and not from local storage

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,4 +1,7 @@
 import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+import { selectCurrentToken } from '../store/users/users.selector';
 
 const api = axios.create({
   baseURL: 'http://localhost:8000/api/v1'
@@ -6,7 +9,7 @@ const api = axios.create({
 
 api.interceptors.request.use(config => {
 
-  const token = localStorage.getItem('token');
+  const token = useSelector(selectCurrentToken);
 
   config.headers.Authorization = `Bearer ${token}`;
   return config;
