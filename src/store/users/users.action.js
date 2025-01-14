dce6e2f2349c2246ba3de3b6374a5dc403703431
unknown <carlosmviegas86@gmail.com>
2025-01-14 15:22:50 +0000

    created a new action to set the jwt token in the redux store instead of the local storage

--- src/store/users/users.action.js
+++ src/store/users/users.action.js
@@ -1,23 +1,28 @@
 import USERS_ACTION_TYPES from './users.type';
 
 const setCurrentUser = user => {
-  return { type: USERS_ACTION_TYPES.SET_CURRENT_USER, payload: user };
+  return { type: USERS_ACTION_TYPES['SET_CURRENT_USER'], payload: user };
+};
+
+const setToken = token => {
+  return { type: USERS_ACTION_TYPES['SET_TOKEN'], payload: token };
 };
 
 const logoutUser = () => {
-  return { type: USERS_ACTION_TYPES.LOGOUT, payload: null };
+  return { type: USERS_ACTION_TYPES['LOGOUT'], payload: null };
 };
 
 const getActiveUsersList = list => {
-  return { type: USERS_ACTION_TYPES.SET_ACTIVE_USERS_LIST, payload: list };
+  return { type: USERS_ACTION_TYPES['SET_ACTIVE_USERS_LIST'], payload: list };
 };
 
 const disableUser = userId => {
-  return { type: USERS_ACTION_TYPES.DISABLE_USER, payload: userId };
+  return { type: USERS_ACTION_TYPES['DISABLE_USER'], payload: userId };
 };
 
 export {
           setCurrentUser,
+          setToken,
           logoutUser,
           getActiveUsersList,
           disableUser
