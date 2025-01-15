42dbcacb5f0427386f45d48d8d0b0ed44ff3c65f
unknown <carlosmviegas86@gmail.com>
2025-01-15 18:15:14 +0000

    added the books reducer to root reducer

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import usersReducer from "./users/users.reducer";
+import booksReducer from "./books/books.reducer";
 
 const rootReducer = combineReducers({
-  users: usersReducer
+  users: usersReducer,
+  books: booksReducer
 });
 
 export default rootReducer;
