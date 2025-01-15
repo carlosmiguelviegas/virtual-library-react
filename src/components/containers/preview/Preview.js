b27fe169c5680fc645fc58a63a581e0a46eb4875
unknown <carlosmviegas86@gmail.com>
2025-01-15 22:29:38 +0000

    implemented the JSX preview component

--- src/components/containers/preview/Preview.js
+++ src/components/containers/preview/Preview.js
@@ -1,5 +1,34 @@
-const Preview = () => {
-  return ;
+import { Fragment, useEffect } from "react";
+
+import { IT_LABEL, SCIENCES_LABEL } from "../../../utils/titles-and-labels";
+import styles from './Preview.module.css';
+
+const Preview = ({ code, books }) => {
+
+  useEffect(() => {
+    mapTitle();
+  }, []);
+
+  const mapTitle = () => {
+    let title;
+    switch (code) {
+      case 'IT':
+        title = IT_LABEL;
+        break;
+      case 'SC':
+        title = SCIENCES_LABEL;
+        break;
+    }
+  };
+
+  return (
+    <Fragment>
+      <h2>{title}</h2>
+      <section className={styles}>
+        { books.map() }
+      </section>
+    </Fragment>
+  );
 };
 
 export default Preview;
